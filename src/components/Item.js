80eda8b6da959f76eb67a0367e2b8f7fb535faa6
iankuria668 <iankuria668@gmail.com>
2024-04-29 09:51:32 +0300

    Add to cart and remove from cart toggle button works

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,11 +2,18 @@ import React, {useState} from "react";
 
 function Item({ name, category }) {
   const [inCart, setInCart] = useState(false);
+  const toggleCart = () => {
+    setInCart(!inCart);
+  };
+  const className = inCart ? "in-cart" : "";
+
   return (
-    <li className="">
+    <li className={className}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick = {toggleCart}>
+        {inCart ? "Remove from Cart" : "Add to Cart"}
+        </button>
     </li>
   );
 }
